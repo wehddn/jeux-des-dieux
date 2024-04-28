57355e1a8740e367b134aaaf3e619b8ee65abc44
Angelina-Graff <graff.aan@gmail.com>
2024-04-28 16:03:51 +0200

    Refacor, starting css

--- src/App.js
+++ src/App.js
@@ -3,34 +3,39 @@ import { Container, Row, Col } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Login from "./pages/page_auth/Login";
+import Signup from "./pages/page_auth/Signup";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { UserAuthContextProvider, useUserAuth } from "./context/UserAuthContext";
 
 function App() {
 
   return (
-    <Container style={{ width: "400px" }}>
-      <Row>
-        <Col>
-          <UserAuthContextProvider>
-            <Routes>
-              <Route
-                path="/home"
-                element={
-                  <ProtectedRoute>
-                    <Home />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="/" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-            </Routes>
-          </UserAuthContextProvider>
-        </Col>
-      </Row>
-    </Container>
+    <div className='App-header'>
+      <div className='App-overlay'></div>
+      <div className='App-content'>
+        <Container>
+          <Row>
+            <Col>
+              <UserAuthContextProvider>
+                <Routes>
+                  <Route
+                    path="/home"
+                    element={
+                      <ProtectedRoute>
+                        <Home />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route path="/" element={<Login />} />
+                  <Route path="/signup" element={<Signup />} />
+                </Routes>
+              </UserAuthContextProvider>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   );
 }
 
