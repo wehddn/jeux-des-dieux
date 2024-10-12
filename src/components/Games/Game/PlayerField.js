f011c61f57db6101211bd22468d03ec92fa1b708
AngeliaGraff <angelina@cashflowpositif.fr>
2024-10-12 19:22:59 +0200

    Curse count

--- src/components/Games/Game/PlayerField.js
+++ src/components/Games/Game/PlayerField.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Card from './Card';
 
 function PlayerField({ player, index, colors, onDropCard, onSlotClick, currentPlayer, slotColors }) {
-  const playerCurses = player.curses || {};
 
   return (
     <div className="player-field">
@@ -10,7 +9,6 @@ function PlayerField({ player, index, colors, onDropCard, onSlotClick, currentPl
         {colors.map((color, slotIndex) => {
           const cardsInSlot = player.table.filter(card => card.slot === slotIndex);
           const className = Object.keys(slotColors)[slotIndex];
-          const curseCount = playerCurses[className] || 0;
           return (
             <div 
               key={slotIndex} 
@@ -22,9 +20,8 @@ function PlayerField({ player, index, colors, onDropCard, onSlotClick, currentPl
             >
               {cardsInSlot.map((card, i) => <Card key={i} card={card} />)}
               <div className="card-count">{cardsInSlot.length}</div>
-              {curseCount > 0 && (
-                <div className="curse-count">ПОРЧА: {curseCount}</div>
-              )}
+              <div>{cardsInSlot.filter(card => card.isCurse === true).length}</div>
+              <div>{cardsInSlot.filter(card => card.isPurification === true).length}</div>
             </div>
           );
         })}
