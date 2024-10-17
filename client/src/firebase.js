b49975be30f5d6f02e5f80b81befcce0a5c18a1e
AngelinaGraff <7530500@mail.ru>
2024-10-17 19:47:14 +0200

    fix

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBP0VkfeaqQxma2-wTAdefz3rp3iNrL9wc",
   authDomain: "jeux-des-dieux.firebaseapp.com",
@@ -15,7 +12,6 @@ const firebaseConfig = {
   measurementId: "G-90015D90T5"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export const auth = getAuth(app);
