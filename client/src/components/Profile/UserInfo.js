9ee83ef837ad9259ad6f7f3582daf2677609db2a
AngelinaGraff <7530500@mail.ru>
2025-01-12 17:37:08 +0100

    App, Profile mobile first

--- client/src/components/Profile/UserInfo.js
+++ client/src/components/Profile/UserInfo.js
@@ -30,7 +30,6 @@ const UserInfo = ({ userProfile }) => {
 
   return (
     <section className="card profile-info d-flex ms-4 mt-4" aria-label="User Information">
-      <div className="p-4">
         <div className="d-flex justify-content-center">
           <img
             className="photoProfil"
@@ -39,19 +38,20 @@ const UserInfo = ({ userProfile }) => {
           />
         </div>
         <div className="d-flex justify-content-start">
-          <h5>Pseudo</h5>
+          <h5 className="profile-tittle">Pseudo</h5>
         </div>
-        <div className="d-flex justify-content-start box-pseudo-input">
+        <div className="d-flex justify-content-start pseudo-input-container">
           {isEditing ? (
-            <div className="d-flex justify-content-start pseudo-input">
+            <div className="d-flex justify-content-start">
               <input
                 type="text"
                 value={newPseudo}
                 onChange={handleInputChange}
                 className="pseudo-input"
+                aria-label="Edit Pseudo"
               />
               <button onClick={handleSaveClick} className="btn-save-pseudo"  aria-label="Save Pseudo">
-                <img src={`/img/btn/save.svg`} alt="Save" className="btn-save-pseudo"/>
+                <img src={`/img/btn/save.svg`} alt="Save" className="btn-save-pseudo-img"/>
               </button>
             </div>
           ) : (
@@ -60,26 +60,25 @@ const UserInfo = ({ userProfile }) => {
                 <p className="m-2">{userProfile.name || "Player"}</p>
               </div>
               <button onClick={handleEditClick} className="btn-edit" aria-label="Edit Pseudo">
-                <img src={`/img/btn/edit.svg`} alt="Edit" className="btn-edit" />
+                <img src={`/img/btn/edit.svg`} alt="Edit" className="btn-edit-img"/>
               </button>
             </>
           )}
         </div>
 
         <div className="pt-3 d-flex justify-content-start">
-          <h5>Mail</h5>
+          <h5 className="profile-tittle">Mail</h5>
         </div>
         <div className="pseudo-container">
           <p className="m-2">{userProfile.email}</p>
         </div>
 
         <div className="pt-3 d-flex justify-content-start">
-          <h5>Numéro Unique</h5>
+          <h5 className="profile-tittle">Numéro Unique</h5>
         </div>
         <div className="pseudo-container container-numero">
           <p className="m-2 word-break">{userProfile.id}</p>
         </div>
-      </div>
     </section>
   );
 };
