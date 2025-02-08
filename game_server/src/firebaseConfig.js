4f3935ca11a31cd0725bf2a366a20bdfa61c06f9
AngelinaGraff <7530500@mail.ru>
2025-02-08 11:18:13 +0100

    Update file structure

--- game_server/src/firebaseConfig.js
+++ game_server/src/firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./jeux-des-dieux-firebase-adminsdk-foy72-8a047523f4.json');
+const serviceAccount = require('./jeux-des-dieux-firebase-adminsdk-foy72-c25aeb4e27.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
