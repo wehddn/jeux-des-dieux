2dc153b68a2834004ce67f07cf0dcc2691a78f8f
AngelinaGraff <7530500@mail.ru>
2025-03-11 19:50:11 +0100

    fix cyrillic

--- db_server/src/controllers/authController.js
+++ db_server/src/controllers/authController.js
@@ -8,12 +8,12 @@ exports.register = async (req, res) => {
     try {
         const { email, password } = req.body;
         if (!email || !password) {
-            return res.status(400).json({ message: 'Все поля обязательны' });
+            return res.status(400).json({ message: 'Email and password are required' });
         }
         
         const [existingUser] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
         if (existingUser.length > 0) {
-            return res.status(400).json({ message: 'Пользователь с таким email уже существует' });
+            return res.status(400).json({ message: 'User already exists' });
         }
         
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -22,10 +22,10 @@ exports.register = async (req, res) => {
         const newUserId = result.insertId;
        
         const token = jwt.sign({ id: newUserId, email }, JWT_SECRET, { expiresIn: '1h' });
-        return res.status(201).json({ token, message: 'Пользователь успешно зарегистрирован' });
+        return res.status(201).json({ token, message: 'User created' });
         } catch (error) {
-        console.error('Ошибка при регистрации:', error);
-        return res.status(500).json({ message: 'Ошибка сервера' });
+        console.error('Error while registering:', error);
+        return res.status(500).json({ message: 'Server error' });
         }
   };
 
@@ -34,24 +34,24 @@ exports.login = async (req, res) => {
     try {
         const { email, password } = req.body;
         if (!email || !password) {
-            return res.status(400).json({ message: 'Email и пароль обязательны' });
+            return res.status(400).json({ message: 'Email and password are required' });
         }
 
         const [userResult] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
         if (userResult.length === 0) {
-            return res.status(400).json({ message: 'Пользователь не найден' });
+            return res.status(400).json({ message: 'User not found' });
         }
         const user = userResult[0];
         
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(400).json({ message: 'Неверный пароль' });
+            return res.status(400).json({ message: 'Wrong password' });
         }
         
         const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: '1h' });
         res.status(200).json({ token });
     } catch (error) {
-        console.error('Ошибка при логине:', error);
-        res.status(500).json({ message: 'Ошибка сервера' });
+        console.error('Error while logging in:', error);
+        res.status(500).json({ message: 'Server error' });
     }
 };
--- db_server/src/middlewares/authMiddleware.js
+++ db_server/src/middlewares/authMiddleware.js
@@ -5,16 +5,16 @@ const JWT_SECRET = process.env.JWT_SECRET || 'your_secret_key';
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     if (!authHeader) {
-        return res.status(403).json({ message: 'Отсутствует токен' });
+        return res.status(403).json({ message: 'No token' });
     }
     
     const token = authHeader.split(' ')[1];
     if (!token) {
-        return res.status(403).json({ message: 'Неверный формат токена' });
+        return res.status(403).json({ message: 'Wrong token' });
     }
     jwt.verify(token, JWT_SECRET, (err, decoded) => {
         if (err) {
-            return res.status(401).json({ message: 'Неверный или просроченный токен' });
+            return res.status(401).json({ message: 'Unauthorized' });
         }
         req.user = decoded;
         next();
